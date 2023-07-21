70ce2f0f6a880698c61d8bef726cbd4ba12edf8b
Eusébio Alves <eusebio.a95@hotmail.com>
2023-07-21 14:25:19 +0100

    Created main 'game round' function
    playRound() now takes two paramenters, user's and cpu's choice, then
    switches the cpu's choice against all possibilities and returns the
    winner!

--- js/script.js
+++ js/script.js
@@ -2,5 +2,45 @@ const possibleChoices = ['Rock', 'Paper', 'Scissors']; //Both the player's and c
 
 let getComputerChoice = () => possibleChoices[Math.floor(Math.random() * 3)]; //Returns a random int from 0-2 as an index for the options.
 
+function playRound(cpuChoice, playerChoice) { //Takes computer and player input 
 
-console.log(getComputerChoice()); 
+    switch (cpuChoice) { //Computer choice is put at test with player's
+
+        case (playerChoice):
+            return 'Tie!';
+            break;
+
+        case ('Rock'):
+            if (playerChoice == 'Paper') {
+                return 'Player wins!';
+            } else {
+                return 'Computer wins!';
+            }
+            break;
+
+        case ('Paper'):
+            if (playerChoice == 'Scissors') {
+                return 'Player wins!';
+            } else {
+                return 'Computer wins!';
+            }
+            break;
+
+        case ('Scissors'):
+            if (playerChoice == 'Rock') {
+                return 'Player wins!';
+            } else {
+                return 'Computer wins!';
+            }
+            break;
+
+        default:
+            return 'Something went wrong!';
+            break;
+
+    }
+
+}
+
+
+console.error(playRound(getComputerChoice(), 'Paper')); 
