9204653e187e960af1131ec7aeb6a15704a3bb31
Eusébio Alves <eusebio.a95@hotmail.com>
2023-07-21 15:35:04 +0100

    Added game function to allow 5 point rounds
    Created a function called game that allows the game to go on up until
    someone hits 5 points and then declares a winner.

--- js/script.js
+++ js/script.js
@@ -3,51 +3,72 @@ const possibleChoices = ['Rock', 'Paper', 'Scissors']; //Both the player's and c
 let getComputerChoice = () => possibleChoices[Math.floor(Math.random() * 3)]; //Returns a random int from 0-2 as an index for the possibleChoices array.
 
 let getPlayerChoice = () => {
-    let playerChoice = prompt('Rock, paper or Scissors?');
+    let playerChoice = prompt('Rock, paper or Scissors?'); //Prompt player for choice
 
-    playerChoice = playerChoice.at(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
+    playerChoice = playerChoice.at(0).toUpperCase() + playerChoice.slice(1).toLowerCase(); //Format players choice to be useful
 
     return playerChoice;
 }
 
-function playRound(cpuChoice, playerChoice) { //Takes computer and player input 
+function game() { //Game function to allow rounds to first 5 points
 
-    switch (cpuChoice) { //Computer choice is put at test with player's
+    let playerScore = 0;
+    let cpuScore = 0;
 
-        case (playerChoice):
-            return 'Tie!';
-            break;
+    function playRound(cpuChoice, playerChoice) { //Takes computer and player input 
 
-        case ('Rock'):
-            if (playerChoice == 'Paper') {
-                return 'Player wins! Paper beats Rock!';
-            } else {
-                return 'Computer wins! Rock beats Scissors!';
-            }
-            break;
+        switch (cpuChoice) { //Computer choice is put at test with player's
 
-        case ('Paper'):
-            if (playerChoice == 'Scissors') {
-                return 'Player wins! Scissors beats Paper!';
-            } else {
-                return 'Computer wins! Paper beats Rock!';
-            }
-            break;
+            case (playerChoice):
+                return 'Tie!';
+                break;
 
-        case ('Scissors'):
-            if (playerChoice == 'Rock') {
-                return 'Player wins! Rock beats Scissors!';
-            } else {
-                return 'Computer wins! Scissors beats Paper!';
-            }
-            break;
+            case ('Rock'):
+                if (playerChoice == 'Paper') {
+                    playerScore++;
+                    return 'Player wins! Paper beats Rock!';
+                } else {
+                    cpuScore++;
+                    return 'Computer wins! Rock beats Scissors!';
+                }
+                break;
 
-        default:
-            return 'Something went wrong!';
-            break;
+            case ('Paper'):
+                if (playerChoice == 'Scissors') {
+                    playerScore++;
+                    return 'Player wins! Scissors beats Paper!';
+                } else {
+                    cpuScore++;
+                    return 'Computer wins! Paper beats Rock!';
+                }
+                break;
+
+            case ('Scissors'):
+                if (playerChoice == 'Rock') {
+                    playerScore++;
+                    return 'Player wins! Rock beats Scissors!';
+                } else {
+                    cpuScore++;
+                    return 'Computer wins! Scissors beats Paper!';
+                }
+                break;
+
+            default:
+                return 'Something went wrong!';
+                break;
+
+        }
 
     }
 
+    while (playerScore < 5 && cpuScore < 5) { //The logic that allows a 5 point game to be played
+        console.log('The player score is: ' + playerScore);
+        console.log('The computer score is: ' + cpuScore);
+        console.warn(playRound(getComputerChoice(), getPlayerChoice()));
+    }
+
+    console.log('Final Score: Player-> ' + playerScore + ' | Computer-> ' + cpuScore); //Shows final score since while exits as soon as someone hits 5 points
 }
 
-console.warn(playRound(getComputerChoice(), getPlayerChoice())); 
+game();
+
